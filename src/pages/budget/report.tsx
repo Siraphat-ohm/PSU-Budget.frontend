9339f74ba28c1a6bde2746933128f41eea403012
Siraphat-ohm <siraphat975@gmail.com>
2023-10-04 13:11:05 +0700

    feat: report

--- src/pages/budget/report.tsx
+++ src/pages/budget/report.tsx
@@ -1,14 +1,28 @@
 import { Layout } from '@/components/Layouts/Layout'
+import useAxiosAuth from '@/lib/hooks/useAxiosAuth'
+import { Typography } from '@mui/material'
+import { Dayjs } from 'dayjs'
 import React from 'react'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 
 type Props = {}
 
+interface IFormInput {
+  startDate: Dayjs,
+  endDate: Dayjs
+}
 const report = (props: Props) => {
+  const axiosAuth = useAxiosAuth();
+  const { handleSubmit, control, formState: { errors, }, reset  } = useForm<IFormInput>({})
+  const onSubmit: SubmitHandler<IFormInput> = async(data) => {};
   return (
     <Layout>
-        <div>report</div>
+      <Typography>รายงาน</Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+      </form>
     </Layout>
   )
 }
 
-export default report
+export default report;
